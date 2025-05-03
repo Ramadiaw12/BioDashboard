74ac89fc7d013e101c777d050edfa08aefc7b8d1
SuperRama <superrama86@gmail.com>
2025-05-03 17:48:05 +0000

    first commitof the dashboard

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import { useState } from 'react'
+
+import './App.css'
+
+function App() {
+  // const [count, setCount] = useState(0)
+
+  return (
+    <>
+      <h1 className='bg-red-300 text-6xl'>Liste des échantillons avec leur statut</h1>
+    </>
+  )
+}
+
+export default App
